20b099cdcf291832a744b39fd6c93c612e66b317
andreasbehnke <ab@andreasbehnke.com>
2023-08-21 19:09:19 +0200

    refined test case

--- src/test/java/de/andreasbehnke/digest/model/WordSequenceComparatorTest.java
+++ src/test/java/de/andreasbehnke/digest/model/WordSequenceComparatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,12 +17,12 @@ class WordSequenceComparatorTest {
         WordSequence seq4 = WordSequence.create("abc def ghi");
 
         List<WordSequence> sequences = Arrays.asList(seq1, seq2, seq3, seq4);
-        List<WordSequence> sorted = sequences.stream().sorted(new WordSequenceComparator()).collect(Collectors.toList());
-        System.out.println(sorted);
+        List<WordSequence> sorted = sequences.stream().sorted(new WordSequenceComparator()).toList();
         assertEquals(seq4, sorted.get(0));
         assertEquals(seq2, sorted.get(1));
         assertEquals(seq3, sorted.get(2));
         assertEquals(seq1, sorted.get(3));
+        assertEquals(4, sorted.size());
     }
 
 }
